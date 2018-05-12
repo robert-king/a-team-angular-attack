cfe6cd70ced002bad38a54b177909e1c711d86b4
giventech <giventech@gmail.com>
2018-05-12 18:33:17 +1000

    added question constants

--- src/app/question/question.service.ts
+++ src/app/question/question.service.ts
@@ -10,7 +10,7 @@ export class QuestionService {
        return globals.questions[this.generate()];
   }  
   generate():number  {
-    return  this.getRandomInt(0, (globals.questions.length -1);
+    return  this.getRandomInt(0, (globals.questions.length -1));
   }
   getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
